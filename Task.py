758d25723da908e9dfaef373fed7f24957863bf2
Hannah M <hannah.e.mcneill@gmail.com>
2015-01-26 11:33:43 -0500

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,10 +1,11 @@
 import datetime
 
 class Task:
-	def __init__(self,name,due,rep,description,sub,stat):
+	def __init__(self,name,due,rep,description,sub,stat,duration):
 		self.name = 
 		self.due = 
 		self.rep = 0
 		self.description = ""
 		self.sub = None
 		self.stat = "Incomplete"
+		self.duration = 0.5
