fa89bb82d5e67cacee565e59ae42a87c53f1f117
Hannah M <hannah.e.mcneill@gmail.com>
2015-01-26 10:19:54 -0500

    1/26/2015

---
+++ Task.py
@@ -0,0 +1,10 @@
+import datetime
+
+class Task:
+	def __init__(self,name,due,rep,description,sub,stat):
+		self.name = 
+		self.due = 
+		self.rep = 0
+		self.description = ""
+		self.sub = None
+		self.stat = "Incomplete"
